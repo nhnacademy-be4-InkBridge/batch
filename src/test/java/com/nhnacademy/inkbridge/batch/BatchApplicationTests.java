e3872cb81de1ef534d305422264eb80a921dabc3
LeeJungBum <jungbum05114@gmail.com>
2024-03-04 15:57:12 +0900

    setting: batch project

---
+++ src/test/java/com/nhnacademy/inkbridge/batch/BatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.inkbridge.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
