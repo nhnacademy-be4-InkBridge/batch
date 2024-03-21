a1bc92da88844eb468b01cc90cc83ec89da9d188
LeeJungBum <jungbum05114@gmail.com>
2024-03-21 14:45:00 +0900

    refactor: 필요없는 어노테이션 제거

--- src/main/java/com/nhnacademy/inkbridge/batch/BatchApplication.java
+++ src/main/java/com/nhnacademy/inkbridge/batch/BatchApplication.java
@@ -7,7 +7,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableBatchProcessing
-@EnableScheduling
 public class BatchApplication {
 
     public static void main(String[] args) {
